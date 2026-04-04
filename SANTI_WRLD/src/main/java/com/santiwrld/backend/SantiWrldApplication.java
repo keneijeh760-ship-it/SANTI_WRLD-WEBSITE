8b3ad0771ec9ee4e7da447fb935863c23168b92a
Kene Ijeh <keneijeh760@gmail.com>
2026-04-04 16:29:44 +0100

    Add Spring Boot project scaffold with User entity and enum

---
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/SantiWrldApplication.java
@@ -0,0 +1,13 @@
+package com.santiwrld.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantiWrldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantiWrldApplication.class, args);
+    }
+
+}
---
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/entities/User.java
@@ -0,0 +1,41 @@
+package com.santiwrld.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.security.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @SequenceGenerator(name = "user",
+    sequenceName = "user",
+    allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user")
+    @Column(unique = true, nullable = false, name = "user_id")
+    private Long Id;
+    @Column(unique = false, nullable = false, name = "firstname")
+    private String firstName;
+    @Column(unique = false, nullable = false, name = "lastname")
+    private String lastName;
+    @Column(unique = true, nullable = false, name = "email")
+    private String email;
+    @Column(unique = true, nullable = true, name = "phoneNumber")
+    private int phoneNumber;
+    @Column(name = "password", nullable = false, unique = true)
+    private String password;
+    @Column(name = "user_role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.CUSTOMER;
+    @Column(nullable = false, unique = false, name = "created_at")
+    private Timestamp createdAt;
+    @Column(nullable = false, unique = false, name = "updated_at")
+    private Timestamp updatedAt;
+}
