d94c2c0de4982363c2ad1178aa099301a008fef2
Kene Ijeh <keneijeh760@gmail.com>
2026-04-04 17:54:57 +0100

    product repository

---
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.santiwrld.backend.repositories;
+
+import com.santiwrld.backend.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findBySlug(String slug);
+    Boolean  findByActiveTrue();
+
+    Optional<Product> findByCollection(String collection);
+    
+}
