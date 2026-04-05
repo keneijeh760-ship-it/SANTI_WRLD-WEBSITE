44fa63873fe30f4ac22df7043f83eb177e8f4494
Kene Ijeh <keneijeh760@gmail.com>
2026-04-05 12:58:31 +0100

    cartItem dto

--- SANTI_WRLD/src/main/java/com/santiwrld/backend/dtos/CartItemDTO.java
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/dtos/CartItemDTO.java
@@ -1,4 +1,17 @@
 package com.santiwrld.backend.dtos;
 
+import com.santiwrld.backend.entities.Product;
+import jakarta.validation.Valid;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CartItemDTO {
+    @Valid
+    private String slug;
+    @Valid
+    private int quantity;
 }
