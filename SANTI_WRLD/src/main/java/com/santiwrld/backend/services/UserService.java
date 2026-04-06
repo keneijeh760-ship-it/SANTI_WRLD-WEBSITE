65e6ebc69e44aa793c69d6ae32fbe769ad027d2b
Kene Ijeh <keneijeh760@gmail.com>
2026-04-06 20:39:01 +0100

    Your message

---
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/services/UserService.java
@@ -0,0 +1,22 @@
+package com.santiwrld.backend.services;
+
+import com.santiwrld.backend.entities.User;
+import com.santiwrld.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User findByEmail (String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        return user;
+    }
+
+
+
+}
