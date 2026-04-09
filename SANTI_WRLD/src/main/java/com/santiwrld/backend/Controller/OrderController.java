11e0fe2d49e847d16b770b139ad4b409de0835cc
Kene Ijeh <keneijeh760@gmail.com>
2026-04-09 22:28:19 +0100

    temp get request order controller

--- SANTI_WRLD/src/main/java/com/santiwrld/backend/Controller/OrderController.java
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/Controller/OrderController.java
@@ -10,10 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +40,19 @@ public class OrderController {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(orderResponse);
+
+    }
+
+    @GetMapping("{reference}")
+    public ResponseEntity<OrderResponseDTO> getOrder(@PathVariable String reference){
+        Order order = orderService.getByReference(reference);
+
+        OrderResponseDTO responseDTO = OrderResponseDTO
+                .builder()
+                .id(order.getId())
+                .orderReference(order.getOrderReference())
+                .customerEmail(order.getCustomerEmail())
+                .totalPrice(order.getTotalPrice())
 
     }
     private List<OrderItemDTO> maptodto (List<OrderItem> orderItems){
