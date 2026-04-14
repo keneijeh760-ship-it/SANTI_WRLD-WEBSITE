8964ff2f21f07b7606299d6a0c2ed5d46aa22bfb
Kene Ijeh <keneijeh760@gmail.com>
2026-04-14 13:41:27 +0100

    global exception handlers

--- SANTI_WRLD/src/main/java/com/santiwrld/backend/ExceptionHandlers/GlobalExceptionHandler.java
+++ SANTI_WRLD/src/main/java/com/santiwrld/backend/ExceptionHandlers/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.santiwrld.backend.ExceptionHandlers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<ErrorResponse> resourceNotFoundException(ResourceNotFound ex){
+        ErrorResponse response = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(RuntimeException ex){
+        ErrorResponse response = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
 }
